08f153dca94bcf0fd3c191ac0e8b4dab6fedc8bc
Dalibor Blazevski <dalibor.blazhevski@students.finki.ukim.mk>
2026-04-16 21:49:13 +0200

    Made some changes at mapper

--- backend/src/main/java/com/example/codeit/model/EarthquakeMapper.java
+++ backend/src/main/java/com/example/codeit/model/EarthquakeMapper.java
@@ -4,6 +4,7 @@ public class EarthquakeMapper {
 
     public static EarthquakeDTO toDto(Earthquake e) {
         EarthquakeDTO dto = new EarthquakeDTO();
+        dto.setId(e.getId());
         dto.setMagnitude(e.getMagnitude());
         dto.setMagType(e.getMagType());
         dto.setPlace(e.getPlace());
